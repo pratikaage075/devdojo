d9faa16800244a5637b4433f6f533b4ba7e3d283
pratikaage075 <pratikaage075@gmail.com>
2026-03-10 12:28:00 +0530

    add cloudflare config

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,8 @@ const withMDX = createMDX({})
 
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  output: 'export',
+  images: { unoptimized: true },
 }
 
 export default withMDX(nextConfig)
